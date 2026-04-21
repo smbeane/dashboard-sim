698669c2fcd7f7477843a676a304d354ebb5e069
Spencer Beane <smbeane5235@gmail.com>
2026-04-21 12:56:42 -0400

    Text renderer

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,13 @@
+#include <string>
+#include <vector>
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-#include <string>
 
 #include <color.hpp>
 #include <renderer.hpp>
-#include <vector>
+#include <fonts.hpp>
+
 
 const int pixel_gap  { 2 };
 const int pixel_size { 18 };
@@ -25,7 +28,58 @@ int main ( int argc, char* args[] ) {
         return 1;
 
     }
-    
+
+    std::string line1 = "abcdefghijklmnop";
+    std::string line2 = "qrstuvwxyz";
+    std::string line3 = "0123456789";
+    int x_start = 0;
+
+    for (int c = 0; c < line1.length(); c++) {
+        uint16_t character1 = font_3x5[line1[c] - 'a'];
+        uint16_t character2, character3;
+
+        if (c < 10) {
+            character2 = font_3x5[line2[c] - 'a'];
+            character3 = font_3x5[line3[c] - 'a'];
+        } else {
+            character2 = 0b000000000000000;
+            character3 = 0b000000000000000;
+        }
+
+        uint16_t mask = 0x4000; 
+
+        for (int j = 0; j < 5; j++) {         
+            for (int i = 0; i < 3; i++) {     
+                
+                int target_index1 = (j * 64) + (x_start + i);
+                int target_index2 = ((j + 6) * 64) + (x_start + i);
+                int target_index3 = ((j + 12) * 64) + (x_start + i);
+
+                if (character1 & mask) {
+                    grid[target_index1] = Color(255, 255, 255);
+                } else {
+                    grid[target_index1] = Color(0, 0, 0);       
+                }
+
+                if (character2 & mask) {
+                    grid[target_index2] = Color(255, 255, 255);
+                } else {
+                    grid[target_index2] = Color(0, 0, 0);       
+                }
+
+                // if (character3 & mask) {
+                //     grid[target_index3] = Color(255, 255, 255);
+                // } else {
+                //     grid[target_index3] = Color(0, 0, 0);       
+                // }
+
+                mask >>= 1; 
+            }
+        }
+
+    x_start += 4; 
+}
+
     bool quit{ false };
     int curr_color = 0;
     bool increase = true;
@@ -41,7 +95,6 @@ int main ( int argc, char* args[] ) {
 
         }
         
-        std::fill(grid.begin(), grid.end(), Color(curr_color, curr_color, curr_color));
 
         render.render_matrix(grid);
 
